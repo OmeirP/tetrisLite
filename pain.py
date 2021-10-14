fb4858f6764dd6159876405f57eece96cf82b1c5
Omeir <op6666667@gmail.com>
2021-10-14 22:47:31 +0100

    placing
    placing appears to work, gravity not yet working though. May use gravity as argument in time.sleep instead of changing how much the pieces move down by. This means I will probably have to change gravity into a float.

--- pain.py
+++ pain.py
@@ -22,16 +22,29 @@ purp=(101, 0, 207)
 
 gravity = 1
 forceDrop = False
-#currYCoord=10
+#currYCoord=20
+currYCoord = None
 
 tetrominoList=["I","Z","S","T"]
 
+#put in function, return nxtPc if same piece keeps repeating
+nxtPc=random.choice(tetrominoList)
 
 floor = pygame.draw.line(gameDisplay, white, (0,520),(800,520),3)
 
 
+def pcGet():
+    currPc=nxtPc
+    return currPc
 
-nxtPc=random.choice(tetrominoList)
+
+
+
+#TPiece=((20,20),(80,20),(80,40),(60,40),(60,60),(40,60),(40,40),(20,40))
+#ZPiece=((20,20),(60,20),(60,40),(80,40),(80,60),(40,60),(40,40),(20,40))
+#SPiece=((20,40),(40,40),(40,20),(80,20),(80,40),(60,40),(60,60),(20,60),(20,40))
+#TPiece=((20,20),(80,20),(80,40),(60,40),(60,60),(40,60),(40,40),(20,40))
+
 
 """
 while True:
@@ -62,19 +75,20 @@ tetrominoDict = {
 
 def tetroPlacer(piece,currYCoord):
     if piece == "I":
-        IPiece=((20,20),(40,20),(40,100),(20,100))
+        IPiece=((20,currYCoord),(40,currYCoord),(40,(currYCoord+80)),(20,(currYCoord+80)))
         drawI = pygame.draw.polygon(gameDisplay, turq, IPiece)
     elif piece == "Z":
-        ZPiece=((20,20),(60,20),(60,40),(80,40),(80,60),(40,60),(40,40),(20,40))
+        ZPiece=((20,currYCoord)),(60,currYCoord),(60,(currYCoord+20)),(80,(currYCoord+20)),(80,(currYCoord+40)),(40,(currYCoord+40)),(40,(currYCoord+20)),(20,(currYCoord+20))
         drawZ = pygame.draw.polygon(gameDisplay, pink,ZPiece)
     elif piece == "S":
-        SPiece=((20,40),(40,40),(40,20),(80,20),(80,40),(60,40),(60,60),(20,60),(20,40))
+        SPiece=((20,(currYCoord+20)),(40,(currYCoord+20)),(40,currYCoord),(80,currYCoord),(80,(currYCoord+20)),(60,(currYCoord+20)),(60,(currYCoord+40)),(20,(currYCoord+40)),(20,(currYCoord+20)))
         drawS = pygame.draw.polygon(gameDisplay, green,SPiece)
     elif piece == "T":
-        TPiece=((20,20),(80,20),(80,40),(60,40),(60,60),(40,60),(40,40),(20,40))
+        TPiece=((20,currYCoord),(80,currYCoord),(80,(currYCoord+20)),(60,(currYCoord+20)),(60,(currYCoord+40)),(40,(currYCoord+40)),(40,(currYCoord+20)),(20,(currYCoord+20)))
         drawT = pygame.draw.polygon(gameDisplay, red,TPiece)
     else:
         print(piece)
+    
         
 #for pixel cut-offs, can draw another shape in the same shape in black pen, and update everytime piece updates? update frame after that?
 
@@ -85,11 +99,17 @@ def tetroPlacer(piece,currYCoord):
 def pieceMover(gravity,forceDrop):
     #change y axis of tetromino by size of 1 'pixel' (gravity) number of times per second
     #for as long as forceDrop is true, and gravity is less than or equal to 5, set gravity to 5
-    tetroPlacer(nxtPc,gravity)
+    if currYCoord is None:
+        tetroPlacer(nxtPc,currYCoord=20)
+    else:
+        while currYCoord < 520:
+            newYCoord=currYCoord+5
+            tetroPlacer(pcGet,currYCoord=newYCoord)
+            time.sleep(gravity)
+    
 
 
-
-
+pieceMover(gravity,forceDrop)
 
 
 
